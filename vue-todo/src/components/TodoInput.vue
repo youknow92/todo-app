de5481e6633d2c0a2834734ad2035cf3948461f1
youknow92 <dbflwla1@gmail.com>
2020-08-16 20:25:30 +0900

    ES6 적ㅇㅇ

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -17,14 +17,14 @@
 <script>
 import Modal from "./common/Modal.vue";
 export default {
-  data: function () {
+  data() {
     return {
       newTodoItem: "",
       showModal: false,
     };
   },
   methods: {
-    addTodo: function () {
+    addTodo() {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
@@ -32,11 +32,11 @@ export default {
         this.showModal = !this.showModal;
       }
     },
-    clearInput: function () {
+    clearInput() {
       this.newTodoItem = "";
     },
   },
-  components: { Modal: Modal },
+  components: { Modal },
 };
 </script>
 
